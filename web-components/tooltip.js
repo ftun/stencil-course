f102d4bffd034ec69bba44a0e056fa59a05f16f0
Felipe Tun <felipe.tun.cauich@gmail.com>
2023-03-27 15:49:46 -0600

    changes

--- web-components/tooltip.js
+++ web-components/tooltip.js
@@ -1,7 +1,9 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        this._tooltipContainer;
+        // this._tooltipContainer;
+        this._tooltipIcon;
+        this._tooltipVisible = false;
         this._tooltipText = 'This is the tooltip text';
         this.attachShadow({ mode: 'open' }); // activar/ abrir shadow DOM
         // Nota: debemos acceder al DOM Virtual mediante this.shadowRoot
@@ -12,14 +14,54 @@ class Tooltip extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <style>
                 div {
+                    font-weight: normal;
                     background-color: black;
                     color: white;
                     position: absolute;
+                    top: 1.5rem;
+                    left: 0.75rem;
                     z-index: 10;
+                    padding: 0.15rem;
+                    border-radius: 3px;
+                    box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
                 }
+
+                .highlight {
+                    background-color: red;
+                }
+
+                /* se pasa un selector entre parentesis */
+                ::slotted(.highlight) {
+                    border-bottom: 2px dotted red;
+                }
+
+                .icon {
+                    background: black;
+                    color: white;
+                    padding: 0.15rem 0.5rem;
+                    text-align: center;
+                    border-radius: 50%;
+                }
+
+                /* Estilos al mismo componente, se pasa un selector entre parentesis */
+                :host(.important) {
+                    /* se accede a la variable declarada en el html{}, y como segundo argumento se puede pasar un valor por default */
+                    background: var(--color-primary, lightcoral);
+                    padding: 0.15rem;
+                }
+
+                /* Estilos al componente dentro de un contexto para hubicarlo (parents), se pasa un selector entre parentesis */
+                :host-context(p) {
+                    font-weight: bold;
+                }
+
+                :host {
+                    position: relative;
+                }
+
             </style>
             <slot>Some default</slot>
-            <span> (?)</span>
+            <span class="icon"> (?)</span>
         `;
     }
 
@@ -31,28 +73,59 @@ class Tooltip extends HTMLElement {
 
 
         // const tooltipIcon = document.createElement('span');
-        const tooltipIcon = this.shadowRoot.querySelector('span');
-        tooltipIcon.textContent = ' (?)';
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-        tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.shadowRoot.appendChild(tooltipIcon);
-        this.shadowRoot.style.position = 'relative';
+        this._tooltipIcon = this.shadowRoot.querySelector('span');
+        this._tooltipIcon.textContent = ' (?)';
+        this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+        // this.shadowRoot.appendChild(this._tooltipIcon); // no es necesario por el slop implementado
+        // this.style.position = 'relative';
+        this._render()
+    }
+
+    // Se invoca/ejecuta cuando se realiza un cambio en los atributos
+    attributeChangedCallback(name, oldValue, newValue) {
+        // console.log('attributeChangedCallback', name, oldValue, newValue)
+        if (oldValue === newValue) {
+            return;
+        }
+
+        if (name === 'text') {
+            this._tooltipText = newValue;
+        }
+        
+    }
+
+    // Se ejecuta cuando se remueve del DOM
+    disconnectedCallback() {
+        console.log('disconnectedCallback')
+        this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+        this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+    }
+
+    _render() {
+        let tooltipContainer = this.shadowRoot.querySelector('div');
+        if (this._tooltipVisible) {
+            tooltipContainer = document.createElement('div');
+            tooltipContainer.textContent = this._tooltipText;
+            this.shadowRoot.appendChild(tooltipContainer);
+        } else {
+            if (tooltipContainer) this.shadowRoot.removeChild(tooltipContainer);
+        }
+    }
+
+    // Se define que atributos se quieren observar en los cambios
+    static get observedAttributes() {
+        return ['text', 'class'];
     }
 
     _showTooltip() {
-        this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = this._tooltipText;
-        // console.log(this._tooltipContainer.style);
-        // this._tooltipContainer.style.backgroundColor = 'black';
-        // this._tooltipContainer.style.color = 'white';
-        // this._tooltipContainer.style.position = 'absolute';
-        // this._tooltipContainer.style.zIndex = '10';
-        this.shadowRoot.appendChild(this._tooltipContainer);
-
+        this._tooltipVisible = true;
+        this._render()
     }
 
     _hideTooltip() {
-        this.shadowRoot.removeChild(this._tooltipContainer);
+        this._tooltipVisible = false;
+        this._render()
     }
 
     // connectedCallback()
